b9f078cb89f549ba0e5982734c8604f45a823b40
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-10-01 13:01:14 +0100

    JS 350

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TV Show Project | My Name (My GitHub username)</title>
+    <link href="style.css" rel="stylesheet" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+  </head>
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  <body>
+    <!--Header-->
+    <div class="header">
+      <img src="./favicon.ico" alt="image of text TV" width="100px" />
+      <span id="available"></span>
+      <div class="header-right">
+        <form id="content">
+          <input type="text" name="input" class="input" id="search-input" />
+          <button type="reset" class="search" id="search-btn"></button>
+        </form>
+      </div>
+    </div>
+    <select
+      name="episodes"
+      onchange="selectFilter()"
+      id="selectEpisode"
+    ></select>
 
-window.onload = setup;
+
+    <div id="root"></div>
+
+    <footer id="footer"><a href="https://www.tvmaze.com/api#licensing" target="_blank">
+        © TVmaze.com </a></footer>
+    
+
+    <!-- Loads a provided function called getAllEpisodes() which returns all episodes -->
+    <script src="episodes.js"></script>
+
+    <!-- Loads YOUR javascript code -->
+    <script src="script.js"></script>
+  </body>
+</html>
